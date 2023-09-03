36de221c6556892c08a9cd4de8de27190a81a64f
Parker Sprouse <parker@parkersprouse.me>
2023-09-03 01:11:20 -0500

    Major progress on FE -> BE action triggering

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,35 +1,83 @@
+// https://tauri.app/v1/guides/features/command
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::api::process::{
-  Command,
-  CommandEvent
+use std::{
+    fs::{File, DirBuilder},
+    io::{
+        BufRead,
+        BufReader,
+        Error,
+        Write
+    }
+};
+use tauri::api::{
+    path,
+    process::{
+        Command,
+        CommandEvent
+    }
 };
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-  let (mut rx, mut _child) = Command::new_sidecar("sass")
-    .expect("failed to create `sass` binary command")
-    .args(["--version"])
-    .spawn()
-    .expect("Failed to spawn `sass` sidecar");
+async fn create_tmp_file(window: tauri::Window, app_handle: &tauri::AppHandle, input: String, percent: i32) -> Result<(), Error> {
+    DirBuilder::new().recursive(true).create(app_handle.path_resolver().app_local_data_dir().unwrap()).unwrap();
 
-  tauri::async_runtime::spawn(async move {
-    while let Some(event) = rx.recv().await {
-      if let CommandEvent::Stdout(line) = event {
-        // child.write("message from Rust\n".as_bytes()).unwrap();
-        println!("{}", line);
-      }
+    let input_file_path = app_handle.path_resolver().app_local_data_dir().unwrap().join("input.scss");
+    let output_file_path = app_handle.path_resolver().app_local_data_dir().unwrap().join("output.css");
+
+    let mut input_file = File::create(&input_file_path)?;
+    write!(input_file, "{}", format!("@use 'sass:color';
+    :root {{
+        $input: {};
+        --output: #{{color.scale($input, $lightness: {}%)}};
+    }}", input, percent))?;
+
+    let (mut rx, mut child) = Command::new_sidecar("sass")
+        .expect("failed to create `sass` binary command")
+        .args([
+            "--no-source-map",
+            &format!("\"{}\"", input_file_path.to_str().unwrap()),
+            &format!("\"{}\"", output_file_path.to_str().unwrap())
+        ])
+        .spawn()
+        .expect("Failed to spawn `sass` sidecar");
+
+    // let result = tauri::async_runtime::spawn(async move {
+    //     while let Some(_event) = rx.recv().await {};
+    // }).await.unwrap();
+    tauri::async_runtime::spawn(async move {
+        while let Some(event) = rx.recv().await {
+            if let CommandEvent::Stdout(line) = event {
+                // println!("{}", line);
+                window
+                    .emit("message", Some(format!("'{}'", line)))
+                    .expect("failed to emit event");
+                child.write("message from Rust\n".as_bytes()).unwrap();
+            }
+        }
+    }).await.expect("Failed to wait for SASS command");
+
+    println!("After the SCSS translation");
+
+    let output_file = File::open(output_file_path)?;
+    let output_contents = BufReader::new(output_file);
+
+    for line in output_contents.lines() {
+        println!("{}", line?);
     }
-  });
 
-  format!("You provided the parameter {}", name)
+    Ok(())
+}
+
+#[tauri::command]
+async fn darken(app_handle: tauri::AppHandle, input: String, percent: i32) -> String {
+    create_tmp_file(&app_handle, input, percent).await.expect("Failed to perform SASS parsing");
+    app_handle.path_resolver().app_local_data_dir().unwrap().display().to_string()
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![darken])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,23 @@
 // https://tauri.app/v1/guides/features/command
-// const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri;
 
-/*
-async function greet() {
-  greetMsgEl.textContent = await invoke('greet', { name: greetInputEl.value });
+async function darken() {
+  const output = document.getElementById('darken_output_field');
+  output.value = await invoke('darken', {
+    input: document.getElementById('input_color').value,
+    percent: 25,
+  });
 }
-*/
 
-window.addEventListener('DOMContentLoaded', () => {
+async function lighten() {
+  const output = document.getElementById('lighten_output_field');
+  output.value = await invoke('lighten', {
+    input: document.getElementById('input_color').value,
+    percent: 25,
+  });
+}
+
+function initInput() {
   const input = document.getElementById('input_color');
   const btn = document.getElementById('picker_trigger');
   window.__APP__.ColorPicker(btn, '#000000');
@@ -15,4 +25,19 @@ window.addEventListener('DOMContentLoaded', () => {
   btn.addEventListener('colorChange', (event) => {
     input.value = event.detail.color.hexa;
   });
+}
+
+function initOutputs() {
+  document
+    .getElementById('darken_output_trigger')
+    .addEventListener('click', darken);
+
+  document
+    .getElementById('lighten_output_trigger')
+    .addEventListener('click', lighten);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  initInput();
+  initOutputs();
 });
