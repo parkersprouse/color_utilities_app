ad1fe1112f20c11b0e453e583213d73561418961
Parker Sprouse <parker@parkersprouse.me>
2023-09-07 01:37:40 -0500

    Switched colorpicker to Alwan

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,41 +1,51 @@
 // https://tauri.app/v1/guides/features/command
+import Alwan from 'alwan';
+
 import { initSliders } from '../lib/slider/slider';
 
 const { invoke } = window.__TAURI__.tauri;
 
-async function initAction(action) {
+let picker;
+
+async function perform(action) {
   const result = await invoke(action, {
-    input: document.getElementById('input_color').value,
+    input: picker.getColor().hex,
     percent: Number.parseFloat(document.getElementById('percent_slider').getAttribute('data-value')),
   });
 
   document.getElementById(`${action}_output_field`).value = result;
-  document.getElementById(`${action}_output_display`).style.backgroundColor =
-    result;
+  document.getElementById(`${action}_output_display`).style.backgroundColor = result;
 }
 
 function initInput() {
-  const input = document.getElementById('input_color');
-  const btn = document.getElementById('picker_trigger');
-  const picker = new window.__APP__.ColorPicker(btn, '#000000');
-
-  btn.addEventListener('colorChange', (event) => {
-    input.value = event.detail.color.hexa;
+  picker = new Alwan('#input_picker', {
+    theme: 'dark',
+    toggle: false,
+    popover: false,
+    position: 'center',
+    preset: false,
   });
-
-  input.addEventListener('input', (event) => {
-    picker.changeColor(event.target.value);
-  });
+  // const input = document.getElementById('input_color');
+  // const btn = document.getElementById('picker_trigger');
+  // const picker = new window.__APP__.ColorPicker(btn, '#000000');
+  //
+  // btn.addEventListener('colorChange', (event) => {
+  //   input.value = event.detail.color.hexa;
+  // });
+  //
+  // input.addEventListener('input', (event) => {
+  //   picker.changeColor(event.target.value);
+  // });
 }
 
 function initOutputs() {
   document
     .getElementById('darken_output_trigger')
-    .addEventListener('click', () => { initAction('darken'); });
+    .addEventListener('click', () => { perform('darken'); });
 
   document
     .getElementById('lighten_output_trigger')
-    .addEventListener('click', () => { initAction('lighten'); });
+    .addEventListener('click', () => { perform('lighten'); });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
